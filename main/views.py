34e7148ee852ecc3c7e0dba47251f2db2707dbfb
TramplingArgumentt <evan.andrian@ui.ac.id>
2026-09-09 14:33:10 +0700

    Add Experience page with Experience model
    Implementasi models ke page.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Experience
+
+def show_main(request):
+    context = {
+        "name": "Evan Andrian",
+        "npm": "2506539082",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "CS student at Universitas Indonesia, often seen at Kantin Pacil on lunch time."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Evan Andrian",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
--- portofolio/views.py
+++ portofolio/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from portofolio.models import Mahasiswa
 
 nama = "Evan Andrian"
 
 mhs1 = "Kak PeBePe"
 
 def landing_page(request):
-    ma = Mahasiswa.objects.all()
 
-    response = {"name": nama, "mahasiswa": ma}
-    return render(request, "index.html", response)
+    return render(request, "index.html")
